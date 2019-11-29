8dd74bd8654a7b2d0af9797721b170e576ed677f
amitoswal <amit1.oswal@gmail.com>
2019-11-29 12:37:49 +0530

    First commit

---
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReviewController {
+
+    @GetMapping("/message")
+    public String getMessage(){
+        return "Hello World";
+    }
+
+    @GetMapping("/newmessage")
+    public String showMessage(){
+        return "Added a new method";
+    }
+}
