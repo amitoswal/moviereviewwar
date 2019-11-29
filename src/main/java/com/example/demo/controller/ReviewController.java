6a0b32c5a1d6e0b6e576f9dde65d91c356bc4348
amitoswal <amit1.oswal@gmail.com>
2019-11-29 12:47:50 +0530

    Added new method for JIRA 12345

--- src/main/java/com/example/demo/controller/ReviewController.java
+++ src/main/java/com/example/demo/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,9 @@ public class ReviewController {
     @GetMapping("/newmessage")
     public String showMessage(){
         return "Added a new method1";
+    }
+    @GetMapping("/message/{name}")
+    public String anotherMessage(@PathVariable("name") String name){
+        return "Hello "+name;
     }
 }
